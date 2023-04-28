53c7464414203c2f384933faf02cab57e98e3441
Elizabeth Karr <e.karr93@icloud.com>
2023-04-28 16:48:43 -0500

    practice generating new compenent

--- angular_practice/component-practice/src/app/app.module.ts
+++ angular_practice/component-practice/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
