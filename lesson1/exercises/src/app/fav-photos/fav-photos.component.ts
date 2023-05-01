4902ece948007388289fc8b19a40a6dfd176060a
Elizabeth Karr <e.karr93@icloud.com>
2023-05-01 12:27:53 -0500

    add more photos and adjust photo size

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQArwmBUZe_Pqm-Pt7TxjgMuKBcdDHDdhccDk5oWgcmZQ&usqp=CAU&ec=48665698';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJZsz_fkER4SZlvT7f675BYPVP-a_Eoe1RJFrOUIwy8kGpAQGPES7afbcuTuMDMKmuR_DsOBaayiU&usqp=CAU&ec=48665698';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIdg7OUarc5V1TdP1hbYR78AUPmUuEeJmNHmdgLvL7kA&usqp=CAU&ec=48665698';
 
   constructor() { }
 
