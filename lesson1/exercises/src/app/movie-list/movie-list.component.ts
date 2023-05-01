ba652b3dceee439604673b901c42acc0913c45af
Elizabeth Karr <e.karr93@icloud.com>
2023-05-01 12:10:39 -0500

    use ng serve to loop through movie list items

--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8', 'Barbie', 'The Little Mermaid'];
+   movies = ['The Manchurian Candidate', 'Oceans 8', 'Barbie', 'The Little Mermaid', 'Glory Road'];
 
    constructor() { }
 
