9aca82c55870309bc02fd59a638031368b3f6c64
Elizabeth Karr <e.karr93@icloud.com>
2023-05-15 15:40:58 -0500

    add event handlers to take off, land, and abort mission buttons

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Exercises: Angular Lesson 3';
+  title = "Exercises: Angular Lesson 3";
 
-  color = 'green';
+  color = "green";
   height = 0;
   width = 0;
-  message = 'Space shuttle ready for takeoff!';
+  message = "Space shuttle ready for takeoff!";
+
+  handleTakeOff() {
+    let result = window.confirm(
+      "Are you sure the shuttle is ready for takeoff?"
+    );
+
+    if (result) {
+      this.color = "blue";
+      this.height = 10000;
+      this.width = 0;
+      this.message = "Shuttle in flight.";
+    }
+  }
+
+  handleLand() {
+    window.alert("The shuttle is landing. Landing gear engaged.");
+
+    this.message = "The shuttle has landed.";
+    this.color = "green";
+    this.height = 0;
+  }
+
+  handleAbort() {
+    let result = window.confirm("Are you sure you want to abort the mission?");
+
+    if (result) {
+      this.message = "Mission aborted.";
+      this.color = "red";
+      this.height = 0;
+    }
+  }
 }
